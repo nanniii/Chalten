e5a64e11f8bbedd5b6cad11983608ddd1f78ec37
WiLL <will@WiLLs-MacPro.local>
2017-02-01 23:38:11 -0500

    First commit-2/1/17

---
+++ app/src/main/java/com/example/will/chalten/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.will.chalten;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private Button button;
+
+    button =
+}
---
+++ app/src/main/java/com/example/will/chalten/importOCV.java
@@ -0,0 +1,9 @@
+package com.example.will.chalten;
+
+/**
+ * Created by will on 2/1/17.
+ */
+
+public class importOCV {
+    static{ System.loadLibrary("opencv_java"); }
+}
