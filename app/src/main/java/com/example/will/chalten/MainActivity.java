5c746d9cca843077c24775e3e334fd25a15db1bd
brianoleary <brian.oleary15@gmail.com>
2017-02-03 16:34:20 -0500

    Commit 2/3/17 4:33 - Added OpenCV camera functionality.

--- app/src/main/java/com/example/will/chalten/MainActivity.java
+++ app/src/main/java/com/example/will/chalten/MainActivity.java
@@ -6,21 +6,62 @@ import android.provider.MediaStore;
 import android.support.v4.media.session.IMediaControllerCallback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final String TAG = "MainActivity";
+    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS: {
+                    mJavaCameraView.enableView();
+                    break;
+                }
+                default: {
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+        }
+    };
 
     private Button mButtonPhoto;
     private Button mButtonDetect;
     private Button mButtonAssign;
 
+    Mat mRgba, mImgGray, mImgCanny;
+
     int num;
 
+    private JavaCameraView mJavaCameraView;
+
     private ImageView mImageView;
+
+    static {
+        if (OpenCVLoader.initDebug()){
+            Log.i(TAG, "OpenCV loaded successfully! :)");
+        }
+        else {
+            Log.i(TAG, "OpenCV did not load successfully! :'(");
+        }
+
+    }
 
 
     @Override
@@ -38,6 +79,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mButtonAssign.setOnClickListener(this);
 
         mImageView = (ImageView) findViewById(R.id.imageView);
+
+        mJavaCameraView = (JavaCameraView) findViewById(R.id.superCamera);
+        mJavaCameraView.setVisibility(SurfaceView.VISIBLE);
+        mJavaCameraView.setCvCameraViewListener(this);
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if (mJavaCameraView!=null){
+            mJavaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected  void onDestroy(){
+        super.onDestroy();
+        if (mJavaCameraView!=null){
+            mJavaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if (OpenCVLoader.initDebug()){
+            Log.i(TAG, "OpenCV loaded successfully! :)");
+            mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+        else {
+            Log.i(TAG, "OpenCV did not load successfully! :'(");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallBack);
+        }
     }
 
     @Override
@@ -67,5 +142,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.assign:
                 break;
         }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mImgGray= new Mat(height, width, CvType.CV_8UC1);
+        mImgCanny = new Mat(height, width, CvType.CV_8UC1);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+
+        Imgproc.cvtColor(mRgba, mImgGray, Imgproc.COLOR_RGB2GRAY);
+        Imgproc.Canny(mImgGray, mImgCanny, 50, 150);
+
+        return mImgGray;
     }
 }
--- openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/will/AndroidStudioProjects/Chalten/openCVLibrary320/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /Users/brianoleary/AndroidStudioProjects/Chalten/openCVLibrary320/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
